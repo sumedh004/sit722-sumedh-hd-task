0868f3a0c789a7a1e411c4c9a59465ca49e5e3f0
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2024-10-10 16:41:17 +1100

    debug

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -3,5 +3,5 @@
 
 
 # Replace with AWS ECR URLs
-docker push 273354648888.dkr.ecr.us-east-1.amazonaws.com/book_catalog/book_catalog:latest
-docker push 273354648888.dkr.ecr.us-east-1.amazonaws.com/inventory_management/inventory_management:latest
+docker push 273354648888.dkr.ecr.us-east-1.amazonaws.com/book_catalog:latest
+docker push 273354648888.dkr.ecr.us-east-1.amazonaws.com/inventory_management:latest
