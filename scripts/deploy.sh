89d884f720f70515227228d4a8b3d8faf5857c5b
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2024-10-10 18:33:23 +1100

    change dockerfile base image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 
-# Substitute the environment variables and deploy
 kubectl apply -f ./scripts/kubernetes/deployment.yaml
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -2,6 +2,5 @@
 
 
 
-# Replace with AWS ECR URLs
 docker push 273354648888.dkr.ecr.us-east-1.amazonaws.com/book_catalog:latest
 docker push 273354648888.dkr.ecr.us-east-1.amazonaws.com/inventory_management:latest
